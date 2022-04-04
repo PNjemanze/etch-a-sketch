eef1539f8975716b2a9bf53836d0266c32c6047f
Princess Sara Njemanze <princesssaracodes@gmail.com>
2022-04-04 16:22:36 -0400

    Created the grid that can change based on dimension input

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let container = document.getElementById("grid");
-for (let i = 0; i < 16; ++i) {
-    let row = document.createElement("div");
-    row.className = "row";
-    for (let j = 0; j < 16; ++j){
-        let col = document.createElement("div");
-        col.className = "column";
-        row.appendChild(col);
+
+// creates square grid with specified dimensions
+function grid(length){
+    
+    for (let i = 0; i < length; ++i) {
+        for (let i = 0; i < length; ++i) {
+            let box = document.createElement('div');
+            box.classList.add('gridBox');
+
+            // sets grid boxes to equal sizes within the grid
+            container.style.gridTemplateRows = `repeat(${length}, 1fr)`;
+            container.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+
+            container.appendChild(box);
+        }
     }
-    container.appendChild(row);
 }
+
+// start etch-a-sketch with a 16 x 16 grid
+grid(16);
