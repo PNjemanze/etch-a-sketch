f8a920a28fc38ff729715f5ff1eefdc73aeb2ce6
Princess Sara Njemanze <princesssaracodes@gmail.com>
2022-04-04 17:53:47 -0400

    Added the option to reset (button) and resize grid (prompt)

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let container = document.getElementById("grid");
 
 // creates square grid with specified dimensions
 function grid(length){
@@ -26,8 +25,31 @@ function hover(){
     }));
 }
 
+function reset(){
+    // allows user to select tile size
+    let dimension = window.prompt("How big do you want the board (4-100)? (eg. 70)");
 
+    // clear grid and sets up new grid with new dimensions
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    grid(dimension);
+
+    // resets grid to original color
+    let gridSq = document.querySelectorAll('div.gridBox');
+    gridSq.forEach(square => {
+            square.style.background = 'white';   
+    });
+
+}
+
+let container = document.getElementById("grid");
+let clear = document.getElementById("clearBtn");
 
 // start etch-a-sketch with a 16 x 16 grid and black pen
 grid(16);
 hover();
+
+// checks to see if user has clicked buttons
+clear.addEventListener('click', reset);
+clear.addEventListener('click', hover);
